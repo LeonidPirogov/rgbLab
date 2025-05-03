f880c98d77d1f58f40f68afe3cf833f3e07cff82
LeonidPirogov <lv.pirogof@gmail.com>
2025-05-03 17:51:29 +0300

    wrote the logic of the application

--- rgbLab/ViewController.swift
+++ rgbLab/ViewController.swift
@@ -9,8 +9,52 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet var displayColorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        displayColorView.layer.cornerRadius = 15
+        
+        redValueLabel.text = string(from: redSlider)
+        greenValueLabel.text = string(from: greenSlider)
+        blueValueLabel.text = string(from: blueSlider)
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func setColor() {
+        displayColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
-
